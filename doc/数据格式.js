dc0758ccea2e06463ca212812c5b3c4ca9e50c72
Git-User-Cherry <736543307@qq.com>
2017-05-21 20:00:52 +0800

    完成目录创建

---
+++ doc/数据格式.js
@@ -0,0 +1,26 @@
+项目名：sex-products
+用户表名 consumers
+{
+	"10000":{
+			"userid":10000,  用户id
+			"nickname":"demo",  用户名
+			"gender":"male",  性别
+			"mobileNo":130900909,  手机号
+			"indent":['12331','123333']  订单号
+	}
+}
+
+商品表名 shopInfo
+{
+	"100000":{
+			"guid":100000,  商品id
+			"title":"WOMO女性粉嫩修复精华",  商品名称
+			"price":99,  现在的价格
+			"ori_price":200,  原价
+			"repertory":1000,  库存
+			"sales":100,  已售
+			"listImg":['./img/01.jpg','./img/02.jpg'], 详情页下面的图片
+			"bannerImg":['./img/11.jpg','./img/12.jpg'], 详情页的banner图
+			"classify":['男性用品','女性用品','安全套']  商品属于什么分类
+	}
+}
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+//引入gulp模块
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//创建gulp任务 以编译sass文件
+gulp.task('compileSass',function(){
+	gulp.src('src/sass/*.scss')
+	//通过pipe方法导入到插件中实现编译sass
+	.pipe(sass({outputStyle:'extended'}).on('error',sass.logError))
+	//把编译的文件输出
+	.pipe(gulp.dest('src/css/'));
+});
+
+//监听文件 自动执行任务
+gulp.task('listenerSass',function(){
+	gulp.watch('src/sass/*.scss',['compileSass']);
+});
+
+//浏览器同步任务
+var browserSync = require('browser-sync');
+gulp.task('autoRefresh',function(){
+	browserSync({
+		//server:'src',
+		//代理服务器
+		proxy:'http://localhost/tps138/',
+		//port:8080,//自定义端口
+		//监听文件修改，自动刷新浏览器
+		files:['src/html/*.html','src/index.html','src/js/*.js']
+	});
+
+	//监听sass文件修改，执行编译sass文件任务
+	gulp.watch('src/sass/*.scss',['compileSass']);
+});
+
+// 用于js文件的压缩
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+gulp.task('compressJS',function(){
+	gulp.src('src/js/*.js')
+
+	// 合并
+	.pipe(concat('page.js'))
+
+	// 输入合并后但未压缩的版本
+	.pipe(gulp.dest('dist/js/'))
+
+	// 压缩
+	.pipe(uglify({
+		mangle:false,
+		compress:false
+	}))
+
+	// 重命名
+	.pipe(rename({
+		suffix:'.min'
+	}))
+
+	// 输出
+	.pipe(gulp.dest('dist/js/'))
+});
