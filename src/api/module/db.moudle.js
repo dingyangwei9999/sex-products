2fbc087c0dd2cb6fe6f2555c5e4de2e76e4aa08e
Git-User-Cherry <736543307@qq.com>
2017-05-22 22:24:22 +0800

    增加后台系统的目录 lib目录下增加一些模块与js文件

---
+++ src/api/module/db.moudle.js
@@ -0,0 +1,51 @@
+var mongodb = require('mongodb');
+
+var server = new mongodb.Server('localhost', 27017);
+
+var db = new mongodb.Db('foo', server);
+
+var exists = function(_collection, data, arr, callback){
+	db.open(function(error, db){
+		if(error){
+			console.log('connect db:', error);
+		}
+		//Account => 集合名（表名）
+		var obj = {};
+		arr.forEach(function (ele) {
+			obj[ele] = data[ele]? data[ele] : '';
+        });
+		
+        db.collection(_collection, function(error, collection){
+            if(error){
+                console.log(error)
+            } else {
+            	console.log('obj:',obj);
+                collection.find(obj).toArray(function(err, docs){
+                    callback(docs);
+                });
+            }
+        });
+        db.close();
+		
+	})	
+};
+
+var save = function(_collection, data){
+	db.open(function(error, db){
+		if(error){
+			console.log('connect db:', error);
+		}
+		//Account => 集合名（表名）
+		db.collection(_collection, function(error, collection){
+			if(error){
+				console.log(error)	
+			} else {
+				collection.insert(data);
+			}
+			db.close();
+		})
+	})
+};
+
+exports.exists = exists;
+exports.save = save;
