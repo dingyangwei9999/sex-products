a7b87c70946743c702384256ceb789724094589f
Git-User-Cherry <736543307@qq.com>
2017-06-03 11:17:06 +0800

    6/3 后台商品列表修改

--- libs/common/global.js
+++ libs/common/global.js
@@ -5,5 +5,5 @@ var base = 'http://10.3.133.20:888/';
 erp.baseUrl = base;
 erp.webappUrl = base+'webapp/';
 erp.htmlUrl = base+'webapp/html/';
-
+erp.uploadUrl = base+'upload/';
 var $main_color = '#FD19AE';
